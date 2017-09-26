1745a1424f0e8ea7273db7d0178dc319afe0efed
VadymPolyanski <vadym.polyanski@gmail.com>
2017-09-26 20:29:56 +0300

    created showing distance result

--- js/distance.handler.js
+++ js/distance.handler.js
@@ -1,16 +1,23 @@
 function calcRoute() {
     var start = document.querySelector('#start').value;
     var end = document.querySelector('#end').value;
-    console.log(start);
-    console.log(end);
+
     var request = {
         origin: start,
         destination: end,
-        travelMode: 'DRIVING'
+        travelMode: 'DRIVING',
+        unitSystem: google.maps.UnitSystem.METRIC
     };
+
     directionsService.route(request, function (result, status) {
         if (status == 'OK') {
+            var route = result.routes[0];
+
+            for (var i = 0; i < route.legs.length; i++) {
+                document.querySelector('.distance').textContent = "Distance: " + route.legs[i].distance.text.replace("км", "km");
+            }
             directionsDisplay.setDirections(result);
         }
     });
+
 }
